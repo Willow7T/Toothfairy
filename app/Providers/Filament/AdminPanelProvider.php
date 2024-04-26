aa9d046d4c9dc4027a94cd91760dba0cdc1c7fdc
Willow <68439913+Willow7T@users.noreply.github.com>
2024-04-27 01:24:11 +0630

    Update Filament package version and fix table.js file

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,7 +42,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Indigo,
                 'secondary' => Color::Gray,
-            ])
+            ])->
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
